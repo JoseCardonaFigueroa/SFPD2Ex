cceeec67550d4c0c4d0a3e4d3a64a60d1f7c4f34
Jose Cardona <cardona5d@gmail.com>
2021-02-12 14:42:56 +0100

    lds examples

--- force-app/main/default/aura/accEdit/accEditController.js
+++ force-app/main/default/aura/accEdit/accEditController.js
@@ -11,5 +11,27 @@
                 console.log('Unkown problem, satate: ' + saveResult.state + ", error: " + JSON.stringify(saveResult.error));
             }
         }));
+    },
+
+    handleRecordupdate: function (component, event, helper) {
+        var eventParams = event.getParams();
+        if(eventParams.changeType === "CHANGED") {
+            // get the fields that are changed for this record
+            var changedFields = eventParams.changedFields;
+            console.log('Fields that are changed: ' + JSON.stringify(changedFields));
+            // record is changed so refresh the component (or other component logic)
+            var resultsToast = $A.get("e.force:showToast");
+            resultsToast.setParams({
+                "title": "Saved",
+                "message": "The record was updated."
+            });
+            resultsToast.fire();
+        } else if(eventParams.changeType === "LOADED") {
+            // record is loaded in the cache
+        } else if(eventParams.changeType === "REMOVED") {
+            // record is deleted and removed from the cache
+        } else if(eventParams.changeType === "ERROR") {
+            console.log('Error: ' + component.get("v.error"));
+        }
     }
 })
